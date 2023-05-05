b352920826f85ac67bccf9f87ae10cf159bc4197
JAYANTH5775 <jayanth.h.s.2002jay@gmail.com>
2023-05-05 06:59:16 +0530

    quiz app added

---
+++ bus2/lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Little intrest or pleasure in doiing things ',
+    [
+      'Not at all',
+      'Several days',
+      'More than half the days',
+      'Nearly every day',
+    ],
+  ),
+  QuizQuestion('Felling down , depressed or hopeless', [
+    'not at all',
+    'several days',
+    'more than half the days',
+    'nearly every day',
+  ]),
+  QuizQuestion(
+    'Trouble  falling asleep , staying asleep or sleeping too much',
+    [
+      'Not at all',
+      'Several days',
+      'More than half the days',
+      'Nearly every day',
+    ],
+   
+  ),
+  QuizQuestion(
+    'felling tries or having little energy',
+    [
+      'Not at all',
+      'Several days',
+      'More than half the days',
+      'Nearly every day',
+    ],
+
+  ),
+  QuizQuestion(
+   'poor appetite or overeating',
+    [
+      'Not at all',
+      'Several days',
+      'More than half the days',
+      'Nearly every day',
+    ],
+  ),
+  QuizQuestion(
+   'feeling bad about yourself or that you are a failure or have let yourself or your family down',
+    [
+      'Not at all',
+      'Several days',
+      'More than half the days',
+      'Nearly every day',
+    ],
+  
+  ),
+];
